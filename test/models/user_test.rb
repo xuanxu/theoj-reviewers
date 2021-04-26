9a0761f72daff3c5db06e1d563dac699d8b88383
Juanjo Bazán <jjbazan@gmail.com>
2021-04-26 19:15:32 +0200

    fix test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "uniqueness of github user" do
-    assert User.create(github: "testuser").valid?
-    assert_not User.create(github: "testuser").valid?
+    assert User.create(github_id: "1", github: "testuser").valid?
+    assert_not User.create(github_id: "2",github: "testuser").valid?
   end
 end
