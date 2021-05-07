0e05e50702acd2d7cb6b45eeef72857586ca0129
Juanjo Bazán <jjbazan@gmail.com>
2021-05-07 12:49:53 +0200

    add editor to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_102724) do
+ActiveRecord::Schema.define(version: 2021_05_07_104511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_102724) do
     t.string "twitter"
     t.string "github_token"
     t.string "github_uid", null: false
+    t.boolean "editor", default: false
     t.index ["github"], name: "index_users_on_github"
     t.index ["last_name"], name: "index_users_on_last_name"
     t.index ["orcid"], name: "index_users_on_orcid"
