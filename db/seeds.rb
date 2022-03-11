386c36f37c44f7e2f544b6d5fa54f55b06a4140f
Juanjo Bazán <jjbazan@gmail.com>
2022-03-11 11:33:10 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+LANGUAGES_LIST = %w(Ada Bash C/C++/C# Clojure CUDA Elixir Erlang Fortran Go Haskell
+                    Java Javascript Julia LaTeX Lisp OpenCL Pascal Perl PHP Python
+                    Mathematica Matlab R Ruby Rust Scala SQL Swift)
+LANGUAGES_LIST.each do |lang|
+  Language.create(name: lang)
+end
