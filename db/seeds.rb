62fa3997895d098da26eb7af0ce0f3ca34a08042
Juanjo Bazán <jjbazan@gmail.com>
2022-08-15 12:42:24 +0200

    comment out seeding Languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 LANGUAGES_LIST = %w(Ada Bash C/C++/C# Clojure CUDA Elixir Erlang Fortran Go Haskell
                     Java Javascript Julia LaTeX Lisp OpenCL Pascal Perl PHP Python
                     Mathematica Matlab R Ruby Rust Scala SQL Swift)
-LANGUAGES_LIST.each do |lang|
-  Language.create(name: lang)
-end
+#LANGUAGES_LIST.each do |lang|
+#  Language.create(name: lang)
+#end
